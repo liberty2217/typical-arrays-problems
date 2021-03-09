60a93d3dba90658f3ee96a4c50042817d6ae7110
Bair <liberty2217@yandex.ru>
2021-03-09 17:50:05 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
 
 exports.min = function min (array) {
-  return (array === undefined || array.length == 0 ? 0 : Math.min.apply(null,array));
+
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    return Math.min(...array);
+    // The new spread operator is a shorter way of writing the apply solution to get the maximum of an array
+  }
+
+  // another way to solve:
+  // let copiedArr = array.slice();
+  // let sortedArr = copiedArr.sort(function(a, b) {
+  //   return a - b;
+  // });
+
+  // return sortedArr[0];
 }
 
 exports.max = function max (array) {
-  return (array === undefined || array.length == 0 ? 0 : Math.max.apply(null,array));
+
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    return Math.max(...array);
+  }
+  
+  // another way to solve:
+  // let currentMax = 0;
+  // for (let i = 0; i < array.length; i++) { 
+  //   let maxValue = Math.max(currentMax, array[i]); 
+  //   if (maxValue > currentMax) {
+  //     currentMax = array[i];
+  //   }
+  // };
+  // return alert(currentMax);  
 }
 
-
 exports.avg = function avg (array) {
-  return (array === undefined || array.length == 0 ? 0 : array.reduce((acum, cur) => acum + cur)/array.length);
+  
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    let sumOfNumbers = array.reduce(function(a, b) {
+      return a + b
+    });
+
+    return sumOfNumbers / array.length;
+  }
 }
